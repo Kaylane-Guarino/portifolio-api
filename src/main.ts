c2beccb19dc4da8d12f3d5cc64c64057957d7e42
Kaylane-Guarino <guarinokaah@gmail.com>
2025-07-26 10:59:46 -0300

    fix: cors authorization

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://kaylane-guarino.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://kaylane-guarino.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-token'],
   });
